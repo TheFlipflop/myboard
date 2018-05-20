571594afb3de97330276594773300d9e0671160e
Flip <philwilluweit@gmail.com>
2018-05-20 15:02:00 +0200

    Updates to threads/replies

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Reply;
+use App\Thread;
 use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Thread $thread)
+    {
+        $thread->addReply([
+            'body' => request('body'),
+            'user_id' => auth()->id()
+        ]);
+
+        return back();
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -27,16 +43,6 @@ class ReplyController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,27 +5,38 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ $thread->title }}</div>
+                    <div class="card-header">
+                        <a href="#">{{ $thread->creator->name }}</a> posted: {{ $thread->title }}
+                    </div>
 
                     <div class="card-body">
                         {{ $thread->body }}
                     </div>
                 </div>
             </div>
-            @foreach($thread->replies as $reply)
-                <div class="col-md-8">
-                    <br>
-                    <div class="card">
-                        <div class="card-header">
-                            <a href="#">
-                                {{ $reply->owner->name }}
-                            </a> said {{ $reply->created_at->diffForHumans() }}...</div>
-                        <div class="card-body">
-                            {{ $reply->body }}
-                        </div>
-                    </div>
-                </div>
-            @endforeach
         </div>
-    </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach($thread->replies as $reply)
+                    @include('threads.reply')
+                @endforeach
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <br>
+                @if(auth()->check())
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        @csrf
+                        <textarea name="body" id="body" class="form-control" placeholder="Your reply"></textarea>
+
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+
+                @else
+                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in forum</p>
+                @endif
+            </div>
+        </div>
+
 @endsection
