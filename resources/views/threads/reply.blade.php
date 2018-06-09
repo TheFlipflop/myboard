85873c02314fa223d3209192046765d65a7cf7f2
Flip <philwilluweit@gmail.com>
2018-06-09 18:06:09 +0200

    updates

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,10 +1,24 @@
-
 <br>
 <div class="card">
     <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a> said {{ $reply->created_at->diffForHumans() }}...</div>
+        <div class="level">
+            <div class="flex">
+                <a href="#">
+                    {{ $reply->owner->name }}
+                </a> said {{ $reply->created_at->diffForHumans() }}...
+            </div>
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    @csrf
+                    <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites_count }} {{ str_plural( 'Favorite', $reply->favorites_count ) }}
+                    </button>
+                </form>
+            </div>
+
+        </div>
+
+    </div>
     <div class="card-body">
         {{ $reply->body }}
     </div>
