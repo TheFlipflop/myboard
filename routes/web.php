895cae4628a857ae7aa1c19cffac324e0eb21f9c
Flip <philwilluweit@gmail.com>
2018-06-10 11:54:30 +0200

    updates

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('threads/create', 'ThreadController@create');
 Route::get('threads/{channel}/{thread}', 'ThreadController@show');
 Route::get('threads/{channel}', 'ThreadController@index');
 Route::post('threads', 'ThreadController@store');
-
 Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
-
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
+
+Route::get('/profiles/{user}', 'ProfilesController@show');
