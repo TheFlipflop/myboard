51df89c2f222064b68a02f4235fb94717c47f306
Flip <philwilluweit@gmail.com>
2018-06-14 16:03:15 +0200

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -47834,10 +47834,15 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             axios.patch('/replies/' + this.attributes.id, {
                 body: this.body
             });
-
             this.editing = false;
-
             flash('Updated!');
+        },
+        destroy: function destroy() {
+            axios.delete('/replies/' + this.attributes.id);
+
+            $(this.$el).fadeOut(300, function () {
+                flash('Your reply has been deleted.');
+            });
         }
     }
 
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -11,7 +11,7 @@
                 <div>
                     <form method="POST" action="/replies/{{ $reply->id }}/favorites">
                         @csrf
-                        <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        <button type="submit" class="btn btn-default btn-sm" {{ $reply->isFavorited() ? 'disabled' : '' }}>
                             {{ $reply->favorites_count }} {{ str_plural( 'Favorite', $reply->favorites_count ) }}
                         </button>
                     </form>
@@ -25,23 +25,17 @@
                 <div class="form-group">
                     <textarea class="form-control" v-model="body" name="" id=""></textarea>
                 </div>
-
                 <button class="btn btn-sm btn-primary" @click="update">Update</button>
                 <button class="btn btn-sm btn-link" @click="editing = false">Cancel</button>
             </div>
-            <div v-else v-text="body"></div>
 
+            <div v-else v-text="body"></div>
         </div>
 
         @can('update', $reply)
             <div class="card-footer level">
                 <button class="button btn-sm mr-1" @click="editing = true">Edit</button>
-                <form method="POST" action="/replies/{{ $reply->id }}">
-                    @csrf
-                    @method('DELETE')
-
-                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                </form>
+                <button class="button btn-danger btn-sm mr-1" @click="destroy">Delete</button>
             </div>
         @endcan
     </div>
